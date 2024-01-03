b5bd078a764c0acede994616663d051606ed4843
pinto0309 <rmsdh122@yahoo.co.jp>
2024-01-04 00:18:55 +0900

    update

--- optimize_reid_tensorrt_engine.sh
+++ optimize_reid_tensorrt_engine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Optimize for up to 10000 ReIDs of 100 x 100.
+# Optimize for up to 10000 ReIDs of 100 x 1.
 # e.g. ./optimize_reid_tensorrt_engine.sh mot17_sbs_S50_NMx3x256x128_post_feature_only.onnx
 
 input_file="$1"
@@ -16,9 +16,7 @@ fi
 start_time=$(date +%s)
 
 for i in {1..100}; do
-    for j in {1..100}; do
-        sit4onnx -if "${input_file}" -fs ${i} 3 ${H} ${W} -fs ${j} 2048
-    done
+    sit4onnx -if "${input_file}" -fs ${i} 3 ${H} ${W} -fs 1 2048
 done
 
 end_time=$(date +%s)
